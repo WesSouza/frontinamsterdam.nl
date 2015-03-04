508c1d2bee6411e0c30bbf2668192e1d41b925f7
Wesley de Souza <wesleywex@gmail.com>
2015-03-04 23:21:57 +0100

    New publish script, gulp-gh-pages was giving us headaches

---
+++ publish.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+GIT_USER=`git config user.name`
+GIT_EMAIL=`git config user.email`
+GIT_REMOTE=`git config remote.origin.url`
+
+rm -rf build || exit 0;
+
+npm install
+
+gulp build
+cd build
+
+git init
+git config user.name "$GIT_USER"
+git config user.email "$GIT_EMAIL"
+
+git add .
+git commit -m "Deploying to GitHub Pages"
+
+git push --force --quiet "$GIT_REMOTE" master:gh-pages
